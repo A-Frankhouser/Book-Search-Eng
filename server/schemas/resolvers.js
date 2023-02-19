b88dd1b043ad8c7c520c4becb41ab80373eaf7d9
Alex Frankhouser <freesoul423@gmail.com>
2023-02-19 11:21:00 -0500

    added addUser, login, saveBook, removeBook mutations to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,6 +17,48 @@ const resolvers = {
 
     // MUTATIONS:
     Mutations: {
-        
+        addUser: async(parent, args ) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async(parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('Wrong email or password, please try again!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Wrong email or password, please try again!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { input}, context) => {
+            if(context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+            return updatedUser;
+            }
+            throw new AuthenticationError('Sorry, you need to be logged in to do this!')
+        },
+        removeBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id},
+                    { $pull: { savedBooks:{ bookId: args.bookId } } },
+                    { new: true }
+                );
+            return updatedUser;
+            }
+            throw new AuthenticationError('Sorry, you need to be logged in to do this!')
+        }
     }
-}
+};
+
+// Exports our resolvers
+module.exports = resolvers;
